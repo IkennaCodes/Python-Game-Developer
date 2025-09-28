1129afa366cdfe4aae691a939d1bd82a92cd66be
IkennaCodes <ikeandadanjoku@gmail.com>
2025-09-28 13:25:19 +0100

    Add files via upload

--- basicshapes.py
+++ basicshapes.py
@@ -1,9 +1,16 @@
 import pgzrun
+linewidth = 10
 def draw():
     screen.clear()
     #Giving bg colour to screen
     screen.fill("yellow")
     screen.draw.circle((230,108),100,color="green")
     screen.draw.text("This is a circle",(100,100),color = "black")
+    screen.draw.line((300, 300), (400, 100), "blue", 6)
+    screen.draw.filled_circle((300,400),50,color = "pink")
+    screen.draw.rect(Rect((400,400),(60,80)),color = "green")
+    screen.draw.rect(Rect((200,200),(80,80)),color = "brown")
+    screen.draw.filled_rect(Rect((100,300),(100,50)),color = "gray")
+    screen.draw.filled_rect(Rect((500,50),(50,50)),color = "white")
 
 pgzrun.go()
---
+++ snowman.py
@@ -0,0 +1,7 @@
+import pgzrun
+def draw():
+    screen.fill("blue")
+    screen.draw.filled_circle((400,100),50, color = "white")
+    screen.draw.filled_circle((400,250),100, color = "white")
+
+pgzrun.go()
