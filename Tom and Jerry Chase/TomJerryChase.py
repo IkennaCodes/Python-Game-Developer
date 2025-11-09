2924b116960bb98377aa7e3e86a69831666cc584
IkennaCodes <ikeandadanjoku@gmail.com>
2025-11-09 13:40:04 +0000

    Update TomJerryChase.py

--- Tom and Jerry Chase/TomJerryChase.py
+++ Tom and Jerry Chase/TomJerryChase.py
@@ -1,12 +1,13 @@
 import random
 import pgzrun
+import time
 
 TITLE = "The Tom & Jerry Chase"
 WIDTH = 600
 HEIGHT = 480
 
 message = ""
-score = 0
+health = 5
 
 tomimage = Actor("tom.png")
 jerryimage = Actor("jerry.png")
@@ -19,9 +20,11 @@ def draw():
     screen.blit("background.png",(0,0))
     tomimage.draw()
     jerryimage.draw()
-    screen.draw.text(str(score), (25,25), color = "white")
+    screen.draw.text(str(health), (25,25), color = "white")
+    screen.draw.text(str(message), (100,25), color = "white")
 
 def update():
+    global health, message
     if keyboard.left:
         jerryimage.x -= 2
     elif keyboard.right:
@@ -30,5 +33,26 @@ def update():
         jerryimage.y -= 2
     elif keyboard.down:
         jerryimage.y += 2
+    elif keyboard.w:
+        tomimage.y -= 2
+    elif keyboard.s:
+        tomimage.y += 2
+    elif keyboard.a:
+        tomimage.x -= 2
+    elif keyboard.d:
+        tomimage.x +=2
+
+    collide = tomimage.colliderect(jerryimage)
+    if collide:
+        health = health - 1
+        time.sleep(0.5)
+        tomimage.x = random.randint(50,550)
+        tomimage.y = random.randint(50,430)
+        message = "Oh No! Run!"
+
+    if health <= 0:
+        message = "Game Over! Tom caught you :("
+        
 
 pgzrun.go()
+
